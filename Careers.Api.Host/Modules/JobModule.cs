7917ed19c783b97ef138b72bb917e7687403816a
Matt <mattlaver@gmail.com>
2015-05-14 20:25:12 +0100

    Tidy up queries

--- Careers.Api.Host/Modules/JobModule.cs
+++ Careers.Api.Host/Modules/JobModule.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Careers.Api.Host.Services;
+﻿using Careers.Api.Host.Services;
 using Nancy;
 
 namespace Careers.Api.Host.Modules
@@ -14,27 +13,11 @@ namespace Careers.Api.Host.Modules
 
             Get["/jobs", runAsync: true] = async (_, token) => Response.AsJson(await _careersService.GetAsync());
 
-          //  Get["jobs/{id}", runAsync: true] = async (param, token) => Response.AsJson(await _careersService.GetSpecAsync(param.));
-
-            // Get["/jobs/", runAsync: true] = async (parameters, ct) => Response.AsJson(await _careersService.GetSpecAsync(ct));
-
-
             Get["/jobs/{id}", runAsync: true] = async (p, token) =>
             {
                 string id = p.id;
                 return Response.AsJson(await _careersService.GetSpecAsync(id));
             };
-
-            //Get["/jobs/{id}"] = parameters =>
-            //{
-            //    var obj = Task.Run(() => _careersService.GetSpecAsync(parameters.id)).Result;
-
-
-            //   // Response.AsText(obj.Details);
-            //};
         }
-
-        
-
     }
 }
--- Careers.Api.Host/Queries/GetJobSpecFromFileQuery.cs
+++ Careers.Api.Host/Queries/GetJobSpecFromFileQuery.cs
@@ -1,18 +1,34 @@
-﻿using System.IO;
+﻿using System.Configuration;
+using System.IO;
+using Castle.Core.Logging;
 
 namespace Careers.Api.Host.Queries
 {
     public class GetJobSpecFromFileQuery : IGetJobSpecQuery
     {
+        private readonly string _dataPath = ConfigurationManager.AppSettings["JobSpecDirectory"];
+        private ILogger _logger = NullLogger.Instance;
+
+        public ILogger Logger
+        {
+            get { return _logger; }
+            set { _logger = value; }
+        }
+        
         public string Execute(string id)
         {
-            const string dataPath = @"C:\easyjet\Careers.Api\Detail";
+            if (!Directory.Exists(_dataPath))
+            {
+                _logger.Error(string.Format("Job Spec directory does not exists: {0}", _dataPath));
+                return string.Empty;
+            }
 
-            if (!Directory.Exists(dataPath)) return string.Empty;
+            var file = _dataPath + "\\" + id + ".md";
 
-            var file = dataPath + "\\" + id + ".md";
-
-            return !File.Exists(file) ? string.Empty : File.ReadAllText(file);
+            if (File.Exists(file)) return File.ReadAllText(file);
+            
+            _logger.Error(string.Format("Job Spec file does not exists: {0}", _dataPath));
+            return string.Empty;
         }
     }
 }
